16add70d5decd900c219544fdbe11e4836561475
Ryan <ryan@neshacker.com>
2023-02-22 21:29:33 -0500

    Cleaned up the math code a little.

--- src/lib/math.s
+++ src/lib/math.s
@@ -1,56 +1,56 @@
 .segment "CODE"
 
 .proc mul16
-  NUM1 = $00
-  NUM2 = $02
-  RESULT = $10
+  num1 = $00
+  num2 = $02
+  result = $10
   lda #0
-  sta RESULT+2
+  sta result+2
   ldx #16
 @loop:
-  lsr NUM2+1      ; Shift a bit
-  ror NUM2
+  lsr num2+1      ; Shift a bit
+  ror num2
   bcc :+          ; 0 or 1 ?
   tay
   clc
-  lda NUM1
-  adc RESULT+2
-  sta RESULT+2
+  lda num1
+  adc result+2
+  sta result+2
   tya
-  adc NUM1+1
-: ror A
-  ror RESULT+2
-  ror RESULT+1
-  ror RESULT
+  adc num1+1
+: ror
+  ror result+2
+  ror result+1
+  ror result
   dex
   bne @loop
-  sta RESULT+3
+  sta result+3
   rts
 .endproc
 
 .proc div8
-  DIVIDEND  = $00
-  DIVISOR   = $01
-  SUB       = $02
-  REM       = $03
+  dividend  = $00
+  divisor   = $01
+  sub       = $02
+  remainder = $03
   ; Zero-out the Remainder memory
   lda #0
-  sta REM
+  sta remainder
   ; for (int x = 8; x > 0; x--)
   ldx #8
 @loop:
   ; Shift the next bit off the front of the dividend
-  asl DIVIDEND
-  rol REM
+  asl dividend
+  rol remainder
   ; Perform the test subtraction
-  lda REM
+  lda remainder
   sec
-  sbc DIVISOR
-  sta SUB
+  sbc divisor
+  sta sub
   bcc @skip_increment
   ; Record a "1" and store the resulting remainder
-  sta REM
-  inc DIVIDEND
+  sta remainder
+  inc dividend
 @skip_increment:
   dex
   bne @loop
@@ -58,41 +58,41 @@
 .endproc
 
 .proc div16
-  DIVIDEND = $00
-  DIVISOR  = $02
-  SUB      = $04
-  REM      = $06
+  dividend  = $00
+  divisor   = $02
+  sub       = $04
+  remainder = $06
   ; Zero-out the Remainder memory
   lda #0
-  sta REM
-  sta REM+1
+  sta remainder
+  sta remainder+1
   ; for (int x = 16; x > 0; x--)
   ldx #16
 @loop:
   ; Shift the next bit off the front of the dividend
-  asl DIVIDEND
-  rol DIVIDEND+1
-  rol REM
-  rol REM+1
+  asl dividend
+  rol dividend+1
+  rol remainder
+  rol remainder+1
 
   ; Perform the test subtraction
-  lda REM
+  lda remainder
   sec
-  sbc DIVISOR
-  sta SUB
+  sbc divisor
+  sta sub
 
-  lda REM+1
-  sbc DIVISOR+1
-  sta SUB+1
+  lda remainder+1
+  sbc divisor+1
+  sta sub+1
 
   bcc @skip_increment
 
   ; Record a "1" and store the resulting remainder
-  ; lda SUB+2
-  sta REM+1
-  lda SUB
-  sta REM
-  inc DIVIDEND
+  ; lda sub+2
+  sta remainder+1
+  lda sub
+  sta remainder
+  inc dividend
 
 @skip_increment:
   dex
@@ -101,50 +101,50 @@
 .endproc
 
 .proc div24
-  DIVIDEND = $00
-  DIVISOR  = $03
-  SUB      = $06
-  REM      = $09
+  dividend  = $00
+  divisor   = $03
+  sub       = $06
+  remainder = $09
   ; Zero-out the Remainder memory
   lda #0
-  sta REM
-  sta REM+1
-  sta REM+2
+  sta remainder
+  sta remainder+1
+  sta remainder+2
   ; for (int x = 24; x > 0; x--)
   ldx #24
 @loop:
   ; Shift the next bit off the front of the dividend
-  asl DIVIDEND
-  rol DIVIDEND+1
-  rol DIVIDEND+2
-  rol REM
-  rol REM+1
-  rol REM+2
+  asl dividend
+  rol dividend+1
+  rol dividend+2
+  rol remainder
+  rol remainder+1
+  rol remainder+2
 
   ; Perform the test subtraction
-  lda REM
+  lda remainder
   sec
-  sbc DIVISOR
-  sta SUB
+  sbc divisor
+  sta sub
 
-  lda REM+1
-  sbc DIVISOR+1
-  sta SUB+1
+  lda remainder+1
+  sbc divisor+1
+  sta sub+1
 
-  lda REM+2
-  sbc DIVISOR+2
-  sta SUB+2
+  lda remainder+2
+  sbc divisor+2
+  sta sub+2
 
   bcc @skip_increment
 
   ; Record a "1" and store the resulting remainder
-  ; lda SUB+2
-  sta REM+2
-  lda SUB+1
-  sta REM+1
-  lda SUB
-  sta REM
-  inc DIVIDEND
+  ; lda sub+2
+  sta remainder+2
+  lda sub+1
+  sta remainder+1
+  lda sub
+  sta remainder
+  inc dividend
 
 @skip_increment:
   dex
